23d0dc98353159fa2fe3c53a9c9392f3b93da315
Marielisa Sierra <89140676+marielisaSierra@users.noreply.github.com>
2022-01-25 15:19:27 +0000

    si

---
+++ modulo1/asignacion/digital-web/ejercicio2.py
@@ -0,0 +1,32 @@
+print("facebook")
+print("digita tu nombre")
+nombre = input()
+print("digita tu apellido")
+apellido = input()
+print("digita tu correo electronico")
+correo = input()
+print("escribe de nuevo la contraseña para validarla")      
+contraseña2 = input()
+print("digita tu mes de nacimiento")
+mes = input()
+print("digita tu dia de nacimiento") 
+dia = int(input())
+fecha = input()
+print("digita tu año de nacieminto")
+año = int(input())
+print("digita tu contraseña")
+contraseña = input()
+
+if (contraseña == contraseña2):
+  print("correcto")
+else:
+ print("la contraseña no coincide")   
+
+
+
+
+
+
+
+
+
---
+++ modulo1/asignacion/ejercicio6.py
@@ -0,0 +1,15 @@
+print("cuanto pagas de local")
+pago_local = int(input() )
+print ("cual es el total de pago por empleado")
+pago_empleado = int(input() )
+print("cuanto gastas de mercancia en un mes aproximadamente")
+pago_mercancia = int(input() )
+print("cuanto generas en un mes aproximadamente")
+ganancias = int(input() )
+
+ganancias1 = print(ganancias - pago_local)
+ganancias2 = print(ganancias1 - pago_empleado)
+ganancias3 = print(ganancias2 - pago_mercancia)
+
+print(ganacias3 * 6)
+print("esta es tu ganancia en 6 meses")
