eddcec3e6be8a41a096640d0e84a156c24f70fc8
Marielisa Sierra <89140676+marielisaSierra@users.noreply.github.com>
2022-01-25 18:11:21 +0000

    cambios

---
+++ modulo1/while/ejemplo1.py
@@ -0,0 +1,20 @@
+# haver un programa que me pida mi edad, no permitir que sea menor de edad, al final imprimir bienvendo
+
+print("dime tu edad")
+edad = int(input())
+
+ while edad < 18:
+print("digita tu edad nuevamnete")
+   edad = int(input())
+   
+print("edad aceptada")   
+
+if edad >= 18:
+  print("a que var te gustaria ir")
+  var = input()
+  print("bievenido")
+else:
+  print("no pudes continuar el programa")
+
+ 
+
