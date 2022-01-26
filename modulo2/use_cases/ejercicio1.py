0f64372cb909ed200ac2992295ccbe753c8e3070
Marielisa Sierra <89140676+marielisaSierra@users.noreply.github.com>
2022-01-26 14:33:47 +0000

    cambios

---
+++ modulo2/use_cases/ejercicio1.py
@@ -0,0 +1,32 @@
+print("digite su id")
+id = int(input() )
+print("digite su nombre de usuario")
+nombre_u = input()
+print("digite su contraseña")
+contraseña = input()
+print("digita tu nombre")
+nombre = input()
+print("digita tu apellido")
+apellido = input()
+
+
+
+print("login \n")
+print("username")
+username = input()
+print("password")
+password = input()
+print("iniciar")
+
+
+if nombre_u == username:
+  print()
+else:
+ print("su nomre de usuario no coincide")  
+
+
+if contraseña == password:
+  print("")
+
+else:
+  print("la contraseña no coincide")  
